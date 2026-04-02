2dd299688b1e702ce132a35bec62db791024a9fe
zanemcmorris <zamc8851@colorado.edu>
2026-04-02 16:37:55 -0600

    Stubs for nvme

--- server/nvmeInterface.c
+++ server/nvmeInterface.c
@@ -3,3 +3,47 @@
     @Author Zane Mcmorris
     @date March 30, 2026
 */
+
+#include "nvmeInterface.h"
+
+/**
+ * @brief Gets information about the interface, drive(s), and namespaces
+ */
+nvmeStatus_t nvmeGetStatus(){
+    return NVME_STATUS_OK;
+}
+
+/**
+ * @brief Create a new NVME namespace
+ */
+nvmeStatus_t nvmeCreateNamespace(){
+    return NVME_STATUS_OK;
+}
+
+/**
+ * @brief Delete an existing NVME namespace. 
+ * @return Returns NVME_STATUS_ERROR if namespace doesn't exist or cannot be deleted
+ *         Otherwise returns NVME_STATUS_OK if operation could be completed
+ */
+nvmeStatus_t nvmeDeleteNamespace(){
+    return NVME_STATUS_OK;
+}
+
+/**
+ * @brief Lists currently available namespaces and information about them
+ */
+nvmeStatus_t nvmeListNamespace(){
+
+    return NVME_STATUS_OK;
+}
+
+/**
+ * @brief Usercommand to write an LBA range in a namespace with some information
+ * TODO: Needs more inputs and implementation.
+ */
+nvmeStatus_t nvmeWriteNamespaceLba(uint32_t ns, lbaRange_t lbaRange){
+    return NVME_STATUS_OK;
+}
+nvmeStatus_t nvmeReadNamespaceLba(uint32_t ns, lbaRange_t lbaRange){
+    return NVME_STATUS_OK;
+}
--- server/nvmeInterface.h
+++ server/nvmeInterface.h
@@ -7,4 +7,23 @@
 #ifndef NVME_INTERFACE_H
 #define NVME_INTERFACE_H
 
+#include "interfaceCommon.h"
+
+typedef enum{
+    NVME_STATUS_OK = 0,
+    NVME_STATUS_ERROR = -1,
+    
+
+}nvmeStatus_t;
+
+nvmeStatus_t nvmeGetStatus();
+
+nvmeStatus_t nvmeCreateNamespace();
+nvmeStatus_t nvmeDeleteNamespace();
+nvmeStatus_t nvmeListNamespace();
+
+nvmeStatus_t nvmeWriteNamespaceLba(uint32_t ns, lbaRange_t lbaRange);
+nvmeStatus_t nvmeReadNamespaceLba(uint32_t ns, lbaRange_t lbaRange);
+
+
 #endif
